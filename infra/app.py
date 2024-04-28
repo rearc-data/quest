35e118b3167fdf849cc00b3e2f7cf20352079347
Sean Owens <seancowens13@gmail.com>
2024-04-28 15:10:27 -0400

    cleaning up a bit

--- infra/app.py
+++ infra/app.py
@@ -2,11 +2,9 @@
 
 import aws_cdk as cdk
 from stacks.rearc_quest_stack import RearcQuestStack
-from stacks.trust_stack import TrustStack
 
 app = cdk.App()
 
-TrustStack(app, "TrustStack")
 RearcQuestStack(app, "RearcQuestStack")
 
 app.synth()
--- infra/tests/unit/test_trust_stack.py
+++ infra/tests/unit/test_trust_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-from stacks.trust_stack import TrustStack
+from stacks.rearc_quest_stack import RearcQuestStack
 
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in trust/trust_stack.py
+# resource in stacks/rearc_quest_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = TrustStack(app, "trust")
+    stack = RearcQuestStack(app, "RearcQuestStack")
     template = assertions.Template.from_stack(stack)
 
 
